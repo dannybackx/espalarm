7c5033cb0f3f859118e85b327467f8d01d903026
dannybackx <dannybackx@users.noreply.github.com>
2017-12-16 10:20:13 +0100

    Add license and copyright

--- Keypad/Oled.cpp
+++ Keypad/Oled.cpp
@@ -1,15 +1,35 @@
 /*
  * Helper class to make coordinates more uniform across operations
+ *
  * For both TFT and Touch, the 0,0 coordinates are in the lower left corner,
  * assuming the VCC pin is on the lower left and T_IRQ is on the lower right.
  *
- * This is a layer above Bodmer's TFT_eSPI.
+ * This is a layer above Bodmer's TFT_eSPI,
+ * which adds functionality to turn on/off the LED,
+ * and screens.
  *
- * adding functionality to turn on/off the LED
- * adding screens
+ * Copyright (c) 2017 Danny Backx
  *
- * Copyright (c) 2017 by Danny Backx
+ * License (MIT license):
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
  */
+
 #include <Oled.h>
 
 const int ctrx = 1;	// Translate coordinates ?
